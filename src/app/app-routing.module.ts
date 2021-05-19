18beab69a6f5e9275b8f6d482730d55a8c740c0a
deseth38 <deseth83@gmail.com>
2021-05-19 04:01:56 +0200

    Update routing and redirection system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,10 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, data: {animation: 'Home'} },
   { path: 'about', component: AboutComponent, data: {animation: 'About'} },
   { path: 'findme', component: FindmeComponent, data: {animation: 'Share'} },
-  { path: '', component: HomeComponent }
+  //{ path: '', component: HomeComponent },
+  //{ path: '**', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
